001e8cc08bdb629c88cba71d90616e085f6ac7f5
MaGo1981 <top.underdog01@gmail.com>
2019-02-23 09:02:50 +0100

    Added menu page, front and back.

--- orders/views.py
+++ orders/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-from .models import Food
+from .models import Food, Pizza, Topping, Sub, Pasta, Salad, Platters
 
 # Create your views here.
 def index(request):
@@ -10,3 +10,15 @@ def index(request):
         "foods": Food.objects.all()
     }
     return render(request, "orders/index.html", context)
+
+def menu(request):
+    context = {
+        "pizzas": Pizza.objects.all(),
+        "toppings": Topping.objects.all(),
+        "subs": Sub.objects.all(),
+        "pastas": Pasta.objects.all(),
+        "salads": Salad.objects.all(),
+        "platters": Platters.objects.all()
+    }
+
+    return render(request, "orders/menu.html", context)
