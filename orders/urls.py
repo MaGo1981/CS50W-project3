8a635f08c288f9fd93314931ad8bacce937ef87b
MaGo1981 <top.underdog01@gmail.com>
2019-02-24 10:26:05 +0100

    Added login and logout pages front and back

--- orders/urls.py
+++ orders/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("menu", views.menu, name="menu")
+    path("menu", views.menu, name="menu"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout")
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,13 +1,17 @@
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 
 from .models import Food, Pizza, Topping, Sub, Pasta, Salad, Platters
 
 # Create your views here.
 def index(request):
+    if not request.user.is_authenticated:
+        return render(request, "orders/login.html", {"message": None})
     context = {
-        "foods": Food.objects.all()
+        "user": request.user
     }
     return render(request, "orders/index.html", context)
 
@@ -22,3 +26,17 @@ def menu(request):
     }
 
     return render(request, "orders/menu.html", context)
+
+def login_view(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "orders/login.html", {"message": "Invalid credentials."})
+
+def logout_view(request):
+    logout(request)
+    return render(request, "orders/login.html", {"message": "Logged out."})
