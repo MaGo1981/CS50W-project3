94ea8d7f784418263dd0569ee3e74ef9bbe44e0c
MaGo1981 <top.underdog01@gmail.com>
2019-02-24 13:09:35 +0100

    Added register page front and back, repeat password not working

--- orders/views.py
+++ orders/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 from .models import Food, Pizza, Topping, Sub, Pasta, Salad, Platters
@@ -9,7 +10,7 @@ from .models import Food, Pizza, Topping, Sub, Pasta, Salad, Platters
 # Create your views here.
 def index(request):
     if not request.user.is_authenticated:
-        return render(request, "orders/login.html", {"message": None})
+        return render(request, "orders/register.html", {"message": None})
     context = {
         "user": request.user
     }
@@ -28,8 +29,8 @@ def menu(request):
     return render(request, "orders/menu.html", context)
 
 def login_view(request):
-    username = request.POST["username"]
-    password = request.POST["password"]
+    username = request.POST.get("username")
+    password = request.POST.get("password")
     user = authenticate(request, username=username, password=password)
     if user is not None:
         login(request, user)
@@ -40,3 +41,16 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return render(request, "orders/login.html", {"message": "Logged out."})
+
+def register_view(request):
+    username1 = request.POST.get("username1")
+    email1 = request.POST.get("email1")
+    password1 = request.POST.get("password1")
+    repeatPassword1 = request.POST.get("repeat-password1")
+    # user1 = User.objects.create_user("alice", "alice@something.com", "alice12345")
+    user1 = User.objects.create_user(username=username1, email=email1, password=password1)
+    if user1 is not None:
+        user1.save()
+        return HttpResponseRedirect(reverse("login"))
+    else:
+        return render(request, "orders/register.html", {"message": "Invalid credentials."})
