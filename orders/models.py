eabf4a1207f2783e2b3d8914718cffc0fbb9fda1
MaGo1981 <top.underdog01@gmail.com>
2019-03-03 09:29:50 +0100

    Added price atribute to Food class, altered print statements for classes to display price.

--- orders/models.py
+++ orders/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Food(models.Model):
     name = models.CharField(max_length=64, default='Dirt')
+    price = models.FloatField(default=2.00)
 
     def __str__(self):
         return f"{self.name}"
@@ -12,7 +13,7 @@ class Topping(Food):
     side = models.CharField(max_length=64, default='whole')
 
     def __str__(self):
-        return f"{self.name} - {self.side}"
+        return f"{self.name} - {self.side}, {self.price} eur "
 
 class Pizza(Food):
     size = models.CharField(max_length=64, default='small')
@@ -21,29 +22,29 @@ class Pizza(Food):
     topping3 = models.ForeignKey(Topping, on_delete=models.CASCADE, related_name="topping3")
 
     def __str__(self):
-        return f"{self.name} - {self.size}, with toppings:{self.topping1}, {self.topping2}, {self.topping3}"
+        return f"{self.name} - {self.size}, with toppings:{self.topping1.name}, {self.topping2.name}, {self.topping3.name}, {self.price} eur"
 
 class Sub(Food):
     size = models.CharField(max_length=64, default='small')
 
     def __str__(self):
-        return f"{self.name} - {self.size}"
+        return f"{self.name} - {self.size}, {self.price} eur"
 
 class Pasta(Food):
     sub1 = models.ForeignKey(Sub, on_delete=models.CASCADE, related_name="sub1")
 
 
     def __str__(self):
-        return f"{self.name} with:{self.sub1.name}"
+        return f"{self.name} with:{self.sub1.name}, {self.price} eur"
 
 class Salad(Food):
 
 
     def __str__(self):
-        return f"{self.name} "
+        return f"{self.name}, {self.price} eur"
 
 class Platters(Food):
     size = models.CharField(max_length=64, default='Antipasto')
 
     def __str__(self):
-        return f"{self.name} - {self.size}"
+        return f"{self.name} - {self.size}, {self.price} eur"
