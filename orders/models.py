e726acef84717a8e3f0ac286eedcfe453b8324da
MaGo1981 <top.underdog01@gmail.com>
2019-03-10 09:29:42 +0100

    Added quantity to food class

--- orders/models.py
+++ orders/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Food(models.Model):
     name = models.CharField(max_length=64, default='Dirt')
     price = models.FloatField(default=2.00)
+    quantity = models.IntegerField(default=1)
 
     def __str__(self):
         return f"{self.name}"
