e1e62689e3e5ca7f807fe1397e781a6ac4d94e6d
MaGo1981 <top.underdog01@gmail.com>
2019-04-09 21:20:12 +0200

    Working on tests. Still having ImportError: Failed to import test module:

--- orders/tests.py
+++ orders/tests.py
@@ -1,9 +1,10 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from django.db.models import Max
 
 from .models import Food, Topping, Pizza
 
 # Create your tests here.
-class ModelsTestCase(TestCase):
+class PizzaTestCase(TestCase):
 
     def setUp(self):
 
@@ -22,6 +23,7 @@ class ModelsTestCase(TestCase):
         Pizza.objects.create(topping1=t1, topping2=t2, topping3=t3)
         Pizza.objects.create(topping1=t1, topping2=t5, topping3=t9)
 
+    # TESTING THE BACKEND SIDE - DATABASE, MODELS, METHODS ETC.
 
     def test1_validPizza(self):
         a1 = Topping.objects.get(name="Oregano",side="whole")
@@ -36,6 +38,33 @@ class ModelsTestCase(TestCase):
         a3 = Topping.objects.get(name="Oregano",side="right")
         p = Pizza.objects.get(topping1=a1, topping2=a2, topping3=a3))
         self.assertTrue(p.is_validPizza())
+
+    # TESTING THE TEMPLATE SIDE - RESPONSES CODES, CONTEXT, ETC.
+
+    def test_menu(self):
+        c = Client()
+        response = c.get("/menu")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["pizzas"].count(), 2)
+
+    def test_valid_food_page(self):
+        a1 = Topping.objects.get(name="Oregano",side="whole")
+        a2 = Topping.objects.get(name="Oregano",side="left")
+        a3 = Topping.objects.get(name="Oregano",side="right")
+        p = Pizza.objects.get(topping1=a1, topping2=a2, topping3=a3))
+
+        c = Client()
+        response = c.get(f"/{p.id}")
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_invalid_food_page(self):
+        max_id = Food.objects.all().aggregate(Max("id"))["id__max"] # We want to get the maximum value of any ID of any flight.
+                                                                    # We want to test what happends if we go to an ID number that does not exist.
+        c = Client()
+        response = c.get(f"/{max_id + 1}")
+        self.assertEqual(response.status_code, 404)
+
 
     # def test_departures_count(self):
     #     a = Airport.objects.get(code="AAA")
