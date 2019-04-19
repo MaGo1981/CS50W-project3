f301ae42ce37f276345243b8f7cb769fae83be82
MaGo1981 <top.underdog01@gmail.com>
2019-04-19 22:29:59 +0200

    Working on testing template side - code responses. Template side tests fail.

--- orders/tests.py
+++ orders/tests.py
@@ -43,50 +43,26 @@ class PizzaTestCase(TestCase):
         p = Pizza.objects.get(topping1=a1, topping2=a2, topping3=a3)
         self.assertFalse(Pizza.is_valid_pizza(p))
 
-    # # TESTING THE TEMPLATE SIDE - RESPONSES CODES, CONTEXT, ETC.
-    #
-    # def test_menu(self):
-    #     c = Client()
-    #     response = c.get("/menu")
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertEqual(response.context["pizzas"].count(), 2)
-    #
-    # def test_valid_food_page(self):
-    #     a1 = Topping.objects.get(name="Oregano",side="whole")
-    #     a2 = Topping.objects.get(name="Oregano",side="left")
-    #     a3 = Topping.objects.get(name="Oregano",side="right")
-    #     p = Pizza.objects.get(topping1=a1, topping2=a2, topping3=a3)
-    #
-    #     c = Client()
-    #     response = c.get(f"/{p.id}")
-    #     self.assertEqual(response.status_code, 200)
+    # TESTING THE TEMPLATE SIDE - RESPONSES CODES, CONTEXT, ETC.
 
+    def test_menu(self):
+        c = Client()
+        response = c.get("menu")
+        self.assertEqual(response.status_code, 200) # AssertionError: 404 != 200; 404 Not found
+        self.assertEqual(response.context["pizzas"].count(), 2)
 
-#     def test_invalid_food_page(self):
-#         max_id = Food.objects.all().aggregate(Max("id"))["id__max"] # We want to get the maximum value of any ID of any flight.
-#                                                                     # We want to test what happends if we go to an ID number that does not exist.
-#         c = Client()
-#         response = c.get(f"/{max_id + 1}")
-#         self.assertEqual(response.status_code, 404)
-
-
-    # def test_departures_count(self):
-    #     a = Airport.objects.get(code="AAA")
-    #     self.assertEqual(a.departures.count(), 3)
-
-    #
-    # def test_arrivals_count(self):
-    #     a = Airport.objects.get(code="AAA")
-    #     self.assertEqual(a.arrivals.count(), 1)
-
-
-    # def test_invalid_flight_destination(self):
-    #     a1 = Airport.objects.get(code="AAA")
-    #     f = Flight.objects.get(origin=a1, destination=a1)
-    #     self.assertFalse(f.is_valid_flight())
-    #
-    # def test_invalid_flight_duration(self):
-    #     a1 = Airport.objects.get(code="AAA")
-    #     a2 = Airport.objects.get(code="BBB")
-    #     f = Flight.objects.get(origin=a1, destination=a2, duration=-100)
-    #     self.assertFalse(f.is_valid_flight())
+    def test_valid_food_page(self):
+        a1 = Topping.objects.get(name="Oregano",side="whole")
+        a2 = Topping.objects.get(name="Onions",side="left")
+        a3 = Topping.objects.get(name="Olives",side="right")
+        p = Pizza.objects.get(topping1=a1, topping2=a2, topping3=a3)
+        food_id=p.id
+        # print(p)
+        # print(p.id)
+        # print(food_id)
+        c = Client()
+        # print(c)
+        # response = c.get(f"/{p.id}") # with this code it returnes ERROR django.urls.exceptions.NoReverseMatch: Reverse for 'card' with arguments '(None,)' not found. 1 pattern(s) tried: ['(?P<user_id>[0-9]+)/card$']
+        response = c.get(food_id)
+        # print(response)
+        self.assertEqual(response.status_code, 200)
