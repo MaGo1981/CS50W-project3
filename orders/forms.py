e954dadce80827e6370d57fdec860422f3b1e456
MaGo1981 <top.underdog01@gmail.com>
2019-05-17 20:55:50 +0200

    Undo last commit.

--- orders/forms.py
+++ orders/forms.py
@@ -1,27 +1,13 @@
 from django import forms
 # from .models import Pasta
 
-SIDE_CHOICES = [
-    ('whole', 'Whole'),
-    ('left', 'Left'),
-    ('right', 'Right')
-
-
 class FoodForm(forms.Form):
     quantity = forms.IntegerField(label="Quantity")
     specialInstructions = forms.CharField(max_length=640, label="Special Instructions")
 
 
 class ToppingForm(FoodForm):
-    # side = forms.CharField(max_length=64, label="Side")
-    side = forms.MultipleChoiceField(
-        required=False,
-        widget=forms.CheckboxSelectMultiple,
-        choices=SIDE_CHOICES,
-        label="Side"
-    )
-
-
+    side = forms.CharField(max_length=64, label="Side")
 
 # class PastaForm(forms.ModelForm):
 #     class Meta:
