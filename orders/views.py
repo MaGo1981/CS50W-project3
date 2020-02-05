ea17c0924c47afd21e460bb6538c35079bfe1f3f
MaGo1981 <top.underdog01@gmail.com>
2020-02-05 01:56:00 +0100

    New Update

--- orders/views.py
+++ orders/views.py
@@ -32,9 +32,9 @@ def item(request, item_id):
 
 
 def v2menu(request):
-    if not request.user.is_authenticated:
-        form = AuthenticationForm()
-        return render(request, "orders/login.html", {"message": "Welcome to Marko's Pizza & Subs! To see our menu page, please login or register!", 'form':form})
+    # if not request.user.is_authenticated:
+    #     form = AuthenticationForm()
+    #     return render(request, "orders/login.html", {"message": "Welcome to Marko's Pizza & Subs! To see our menu page, please login or register!", 'form':form})
 
     context = {
         # "items": Item.objects.all().order_by('_food___menuPosition'), # extra dunder between fields
